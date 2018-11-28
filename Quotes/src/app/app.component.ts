649652ef26a5a4bc9ff673aa6f46f628e73ef534
BenOkwara <maranathabenson@gmail.com>
2018-11-28 07:08:51 +0300

    Add quote component that will handle the quotes logic

--- Quotes/src/app/app.component.ts
+++ Quotes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes = [
+    new Quote(1, 'Our uniqueness makes us special, makes perception valuable - but it can also make us lonely. This loneliness is different from being Alone: You can be lonely even surrounded by people. The feeling Im talking about stems from the sense that we can never fully share the truth of who we are. I experienced this acutely at an early age.'),
+    new Quote(2, 'In everyone’s life, at some time, our inner fire goes out. It is then burst into flame by an encounter with another human being. We should all be thankful for those people who rekindle the inner spirit.'),
+    new Quote(3, 'Simplicity is the most difficult thing to secure in this world; it is the last limit of experience and the last effort of genius.'),
+    new Quote(4, 'Carrying the cross does mean following in Jesus footsteps. And in His footsteps are rejection, brokenheartedness, persecution and death. There are not two Christs - an easy going one for easy going Christians, and a suffering one for exceptional believers. There is only one Christ. Are we willing to follow His lead?'),
+    new Quote(5, 'The Great Commission is not an option to be considered; it is a command to be obeyed.'),
+  ];
+
+  constructor() {
+
+  }
 }
